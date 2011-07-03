54433b832117131f8c4cfafdb8a0e13ddf543b11
Oren Robinson <get.oren@gmail.com>
2011-07-03 16:41:26 -0400

    added jquery test, unsuccessful so far...

--- coffee/test.coffee
+++ coffee/test.coffee
@@ -1,7 +1,20 @@
+log = (txt) ->
+    console.log txt
+
 get_weekday = (num) ->
   days = ['m','t','w','t','f','s','s']
   days[num]
 
-weekdays = (get_weekday num for num in [2..4])
+toggle = (elem) ->
+	switch elem.css 'float'
+		when 'right' then elem.css 'float' 'left'
+  		when 'left' then elem.css 'float' 'right'
+  		else elem.css 'float' 'right'
 
-console.log weekday for weekday in weekdays
+main = ->
+	$('#content').click =>
+		toggle(this)
+	weekdays = (get_weekday num for num in [2..4])
+	log weekday for weekday in weekdays
+	
+main()
--- js/test.js
+++ js/test.js
@@ -1,20 +1,29 @@
 (function() {
-  var get_weekday, num, weekday, weekdays, _i, _len;
+  var get_weekday, log, main;
+  log = function(txt) {
+    return console.log(txt);
+  };
   get_weekday = function(num) {
     var days;
     days = ['m', 't', 'w', 't', 'f', 's', 's'];
     return days[num];
   };
-  weekdays = (function() {
-    var _results;
+  main = function() {
+    var num, weekday, weekdays, _i, _len, _results;
+    weekdays = (function() {
+      var _results;
+      _results = [];
+      for (num = 2; num <= 4; num++) {
+        _results.push(get_weekday(num));
+      }
+      return _results;
+    })();
     _results = [];
-    for (num = 2; num <= 4; num++) {
-      _results.push(get_weekday(num));
+    for (_i = 0, _len = weekdays.length; _i < _len; _i++) {
+      weekday = weekdays[_i];
+      _results.push(log(weekday));
     }
     return _results;
-  })();
-  for (_i = 0, _len = weekdays.length; _i < _len; _i++) {
-    weekday = weekdays[_i];
-    console.log(weekday);
-  }
+  };
+  main();
 }).call(this);
